7609621b784eab7689757aa5006570c3243982a5
jun HU <jhu@SDSWKS0017.(none)>
2014-08-29 15:09:36 -0400

    update run_service

--- run_service.py
+++ run_service.py
@@ -2,4 +2,9 @@ import os
 from anchovy import create_app
 from flask.ext.script import Manager, Shell
 
-app = create
+app = create_app(os.getenv('CONFIG') or 'default')
+manager = Manager(app)
+
+
+if __name__ == '__main__':
+    manager.run()
