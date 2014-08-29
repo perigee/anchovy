e5828f9a8f6b903e80a8caa4385e9232ce99226d
jun HU <jhu@SDSWKS0017.(none)>
2014-08-29 14:04:53 -0400

    add app init file

--- anchovy/__init__.py
+++ anchovy/__init__.py
@@ -1,3 +1,12 @@
-__all__ = ["oauth"]
-
 from anchovy.oauth import oauth 
+from flask import Flask
+import os
+
+def create_app(config=None):
+    app = Flask(__name__)
+    if config is None:
+        config = os.path.join(app.root_path, 'default.cfg')
+    
+    app.config.from_pyfile(config)
+    
+    return app
