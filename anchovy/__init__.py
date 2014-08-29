9566d8f35152b36b543c89d5995b4616be98d805
jun HU <jhu@SDSWKS0017.(none)>
2014-08-29 15:03:36 -0400

    add moment, mail, bootstrap

--- anchovy/__init__.py
+++ anchovy/__init__.py
@@ -1,9 +1,26 @@
-from anchovy.oauth import oauth 
+
 from flask import Flask
+from flask.ext.bootstrap import Bootstrap
+from flask.ext.moment import Moment
+from flask.ext.mail import Mail
+from config import config
 import os
+import logging
+
+
+bootstrap = Bootstrap()
+moment = Moment()
+mail = Mail()
+
 
 def create_app(config=None):
     app = Flask(__name__)
+    
+    bootstrap.init_app(app)
+    mail.init_app(app)
+    moment.init_app(app)
+
+
     if config is None:
         config = os.path.join(app.root_path, 'default.cfg')
     
